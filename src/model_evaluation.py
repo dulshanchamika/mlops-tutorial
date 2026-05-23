8d5eb42c29d9badc1d22d3d5d7e35092a0efdf1a
Dulshan Chamika <dulshanchamika2@gmail.com>
2026-05-23 23:56:22 +0530

    first commit

---
+++ src/model_evaluation.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+
+import pickle
+import json
+
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    roc_auc_score
+)
+
+clf = pickle.load(open('model.pkl', 'rb'))
+test_data = pd.read_csv('./data/features/test_bow.csv')
+
+X_test = test_data.iloc[:, 0:-1].values
+y_test = test_data.iloc[:, -1].values
+
+y_pred = clf.predict(X_test)
+
+# Predict probabilities
+y_pred_proba = clf.predict_proba(X_test)
+
+# Get probability for positive class
+positive_class_index = 1
+
+y_pred_proba_positive = y_pred_proba[:, positive_class_index]
+
+# Calculate evaluation metrics
+accuracy = accuracy_score(y_test, y_pred) # type: ignore
+
+precision = precision_score(
+    y_test, # type: ignore
+    y_pred,
+    pos_label=1
+)
+
+recall = recall_score(
+    y_test, # type: ignore
+    y_pred,
+    pos_label=1
+)
+
+auc = roc_auc_score(
+    y_test, # type: ignore
+    y_pred_proba_positive
+)
+
+metrics_dict = {
+    'accuracy': accuracy,
+    'precision': precision,
+    'recall': recall,
+    'auc': auc
+}
+
+with open('metrics.json', 'w') as file:
+    json.dump(metrics_dict, file, indent=4)
+
+print(metrics_dict)
